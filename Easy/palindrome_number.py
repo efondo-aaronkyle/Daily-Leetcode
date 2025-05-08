de0686cb18d59f26736a025d960642ba42e07019
efondo_aaronkyle <akyleefondo@gmail.com>
2025-05-08 22:47:34 +0800

    add is palindrome function

---
+++ Easy/palindrome_number.py
@@ -0,0 +1,10 @@
+def is_palindrome(number):
+    original_number, reverse_number = number, 0 
+
+    if number < 0:
+        return False
+
+    while number > 0:
+        reverse_number = reverse_number * 10 + number % 10
+        number //= 10
+    return original_number == reverse_number
