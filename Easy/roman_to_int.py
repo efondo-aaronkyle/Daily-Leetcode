c9cf4320d4d5cf1df1254b8324d4c2fb5a2231ec
efondo_aaronkyle <akyleefondo@gmail.com>
2025-05-08 23:31:35 +0800

    add roman to integer function

---
+++ Easy/roman_to_int.py
@@ -0,0 +1,19 @@
+def roman_to_integer(roman_numeral):
+    result = 0
+    roman_to_int = {
+        'I': 1, 
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    }
+
+    for roman in range(len(roman_numeral)):
+        if roman + 1 < len(roman_numeral) and roman_to_int[roman_numeral[roman]] < roman_to_int[roman_numeral[roman + 1]]:
+            result -= roman_to_int[roman_numeral[roman]]
+        else:
+            result += roman_to_int[roman_numeral[roman]]
+    
+    return result
