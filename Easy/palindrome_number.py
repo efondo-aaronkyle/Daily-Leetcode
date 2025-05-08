ce73314ec444f41c6a6fbe826db191ac634082ab
efondo_aaronkyle <akyleefondo@gmail.com>
2025-05-08 22:48:09 +0800

    add output

--- Easy/palindrome_number.py
+++ Easy/palindrome_number.py
@@ -8,3 +8,7 @@ def is_palindrome(number):
         reverse_number = reverse_number * 10 + number % 10
         number //= 10
     return original_number == reverse_number
+
+print(is_palindrome(121))
+print(is_palindrome(-121))
+print(is_palindrome(10))
