8dee1f50d66c65eae16fe6122f96ddaecf49dc2c
efondo_aaronkyle <akyleefondo@gmail.com>
2025-05-08 22:25:17 +0800

    add output

--- Easy/two_sum.py
+++ Easy/two_sum.py
@@ -3,3 +3,8 @@ def two_sum(nums, target):
         for second_index in range(first_index + 1, len(nums)):
             if nums[first_index] + nums[second_index] == target:
                 return [first_index, second_index]
+            
+
+print(two_sum([2,7,11,15], 9))
+print(two_sum([3,2,4], 6))
+print(two_sum([3,3], 6))
